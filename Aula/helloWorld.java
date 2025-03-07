92c54c3174cbb7f63dc2b352cb5e99ff509adf9f
MathIsTuff <mathistuff@gmail.com>
2025-03-07 17:37:00 -0300

    Update helloWorld.java

--- Aula/helloWorld.java
+++ Aula/helloWorld.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+		ola()
     }
+	public static void ola(){
+        System.out.println("Olá Mundo!");
+    }
+
 }
